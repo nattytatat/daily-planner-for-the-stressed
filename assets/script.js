33ec6d627bf249f99617017b4792ccafec2b8e59
Natalie <natalie@bigorangemedia.co.uk>
2023-01-20 20:07:25 +0000

    fix container variable, test loop, change timestart

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,12 @@ var today = moment().format("[Today is - ] dddd, Do MMMM YYYY");
 // time now in 24 hour clock
 var timeNow = moment().format("kk:mm:ss a");
 // Define the working hours
-var dayStart = moment().hours(9).minutes(0).seconds(0);
-var dayEnd = moment().hours(17).minutes(0).seconds(0);
+var dayStart = 9;
+// var dayStart = moment().hours(9).minutes(0).seconds(0);
+// var dayEnd = moment().hours(17).minutes(0).seconds(0);
 // current hour
 var currentHour = moment().hours();
-var container = $('#container');
+var container = $('.container');
 // What the user types
 // var theEventInput = $("");
 
@@ -16,19 +17,30 @@ $("#currentDay").text(today);
 
 // console.log(timeNow);
 
+// for (var i = dayStart; i <= 17; i++) {
+//     console.log("Each hour " + i);
+// }
+
 // List the current working hours of the day in timeblock
-for (var i = currentHour; i < 17; i++) {
+for (var i = dayStart; i < 17; i++) {
     // dynamically create HTML elements for each hour
     var theRow = $('<div>').addClass('row');
     var theHour = $('<div>').addClass('hour col-2');
-    var timeBlock = $('<div>').addClass('time-block col-10');
-    var textarea = $('textarea');
+    var timeBlock = $('<div>').addClass('time-block col-8');
+    var textarea = $('<textarea>').addClass('col-12');
+    var saveBtn = $('<button><i class="fas fa-save"></i>').addClass('saveBtn col-2');
 
+    var hourFormat = moment().hours(i).minutes(0).seconds(0).format("h:mm a");
+    theHour.text(hourFormat);
+    textarea.attr("id", hourFormat);
+    // textarea.attr("id", i+":00");
+    
     theRow.append(
         theHour,
         timeBlock,
-        theHour
+        saveBtn
     );
+    timeBlock.append(textarea);
 
     theRow.appendTo(container);
   }
