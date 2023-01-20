e55c740577e0584530a5e110d699d369a66b25a8
Natalie <natalie@bigorangemedia.co.uk>
2023-01-20 20:13:39 +0000

    reorganise appended elements

--- assets/script.js
+++ assets/script.js
@@ -26,21 +26,22 @@ for (var i = dayStart; i < 17; i++) {
     // dynamically create HTML elements for each hour
     var theRow = $('<div>').addClass('row');
     var theHour = $('<div>').addClass('hour col-2');
-    var timeBlock = $('<div>').addClass('time-block col-8');
-    var textarea = $('<textarea>').addClass('col-12');
+    var timeBlock = $('<div>').addClass('time-block col-12');
+    var textArea = $('<textarea>').addClass('col-8');
     var saveBtn = $('<button><i class="fas fa-save"></i>').addClass('saveBtn col-2');
 
     var hourFormat = moment().hours(i).minutes(0).seconds(0).format("h:mm a");
     theHour.text(hourFormat);
-    textarea.attr("id", hourFormat);
+    textArea.attr("id", hourFormat);
     // textarea.attr("id", i+":00");
     
     theRow.append(
-        theHour,
         timeBlock,
+        theHour,
+        textArea,
         saveBtn
     );
-    timeBlock.append(textarea);
+    textArea.append(timeBlock);
 
     theRow.appendTo(container);
   }
