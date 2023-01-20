fd77d1dc30b604765f6702b79b1ef64897e2f872
Natalie <natalie@bigorangemedia.co.uk>
2023-01-20 19:40:28 +0000

    start for loop and dynamic HTML elements

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,11 @@ var timeNow = moment().format("kk:mm:ss a");
 // Define the working hours
 var dayStart = moment().hours(9).minutes(0).seconds(0);
 var dayEnd = moment().hours(17).minutes(0).seconds(0);
+// current hour
+var currentHour = moment().hours();
+var container = $('#container');
 // What the user types
-var theEventInput = $("");
+// var theEventInput = $("");
 
 // print date to page
 $("#currentDay").text(today);
@@ -14,6 +17,21 @@ $("#currentDay").text(today);
 // console.log(timeNow);
 
 // List the current working hours of the day in timeblock
-// for () {
+for (var i = currentHour; i < 17; i++) {
+    // dynamically create HTML elements for each hour
+    var theRow = $('<div>').addClass('row');
+    var theHour = $('<div>').addClass('hour col-2');
+    var timeBlock = $('<div>').addClass('time-block col-10');
+    var textarea = $('textarea');
 
-// }
+    theRow.append(
+        theHour,
+        timeBlock,
+        theHour
+    );
+
+    theRow.appendTo(container);
+  }
+  
+
+// event listener for saveBtn
